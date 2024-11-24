106f5d26cf50c8e48950cc14cb21a93171b39c76
sidongit1234 <siddeshganapathy@gmail.com>
2024-11-24 15:54:26 -0800

    2nd commit

--- password_vault.py
+++ password_vault.py
@@ -4,17 +4,48 @@ def create_vault(db_file):
     cursor = conn.cursor()
     cursor.execute('''
                    CREATE TABLE IF NOT EXISTS PASSWORDS(
-                   id INTEGER PRIMARY KEY
-                   service TEXT
-                   username TEXT
-                   password TEXT 
+                   id INTEGER PRIMARY KEY,
+
+                   service TEXT,
+
+                   username TEXT,
+
+                   password TEXT,
+
                    date_added TIMESTAMP
+            
                    )
                    
                    ''')
 
     conn.commit()
+
     conn.close()
+
+def password_vault_menu():
+
+    print("/nPassword Vault Menu")
+
+    print("1. Add password")
+
+    print("2. Remove Password")
+          
+    print("3. View Password")
+
+    print("4. Change password")
+
+    print("5. Exit")
+          
+
 def main():
     db_file = 'password_vault.db'
+
+    create_vault(db_file)
+
+    print("Welcome to the password vault")
+
+    password_vault_menu()
  
+if __name__ == "__main__":
+
+    main()
